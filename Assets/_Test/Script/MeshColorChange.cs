2e81e207ec9f3feb231fda832aae1b911efbf9e0
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2025-03-06 00:50:57 +0700

    Update

---
+++ Assets/_Test/Script/MeshColorChange.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.Serialization;
+
+namespace _Test.Script
+{
+    using UnityEngine;
+
+    public class MeshColorChanger : MonoBehaviour
+    {
+        public Color objectColor = Color.white; // Exposed color variable
+
+        [FormerlySerializedAs("meshRenderer")] [SerializeField]
+        private MeshRenderer _meshRenderer;
+        
+        private Material objectMaterial;
+
+        void Start()
+        {
+
+            // Ensure we don't modify the shared material
+            objectMaterial = _meshRenderer.material;
+            objectMaterial.color = objectColor;
+        }
+
+        void Update()
+        {
+            // Apply color change in real-time
+            objectMaterial.color = objectColor;
+        }
+    }
+
+}
